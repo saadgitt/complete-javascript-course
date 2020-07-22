e4a99987497b7e1d5c1420ae60049d10e2a2bc57
saadgitt <saad.akbar@tintash.com>
2020-07-22 18:00:39 +0500

    Likes.js : added the likes class or model

--- 9-forkify/final/src/js/models/Likes.js
+++ 9-forkify/final/src/js/models/Likes.js
@@ -1,42 +1,24 @@
+//Saad's file to save likes
 export default class Likes {
     constructor() {
-        this.likes = [];
+        this.likes = []; //to store likes
     }
-
-    addLike(id, title, author, img) {
-        const like = { id, title, author, img };
-        this.likes.push(like);
-
-        // Perist data in localStorage
-        this.persistData();
-
-        return like;
+    addLike(id, title, author, img) { //since we need all of this to display likes.
+         const like = {id, title,author, img};
+         this.like.push(like); //store that like 
+         return like;
     }
-
-    deleteLike(id) {
+    deleteLike(id)
+    {
         const index = this.likes.findIndex(el => el.id === id);
-        this.likes.splice(index, 1);
-
-        // Perist data in localStorage
-        this.persistData();
+        this.likes.splice(index, 1); //removes likes.
     }
-
-    isLiked(id) {
-        return this.likes.findIndex(el => el.id === id) !== -1;
+    isLiked(id)
+    {
+        return this.likes.findIndex(el => el.id === id) !== -1; //returns false if id not found
     }
-
-    getNumLikes() {
-        return this.likes.length;
-    }
-
-    persistData() {
-        localStorage.setItem('likes', JSON.stringify(this.likes));
-    }
-
-    readStorage() {
-        const storage = JSON.parse(localStorage.getItem('likes'));
-        
-        // Restoring likes from the localStorage
-        if (storage) this.likes = storage;
+    getNumLikes()
+    {
+        return this.likes.length; //likes count 
     }
 }
