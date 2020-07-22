51b0eadb07132c7814408dc71d1cc74e3ea3bb5d
saadgitt <saad.akbar@tintash.com>
2020-07-22 18:03:18 +0500

    likesViews.js: added the likes view successfully

--- 9-forkify/final/src/js/views/likesView.js
+++ 9-forkify/final/src/js/views/likesView.js
@@ -1,17 +1,15 @@
 import { elements } from './base';
-import { limitRecipeTitle } from './searchView';
 
-export const toggleLikeBtn = isLiked => {
-    const iconString = isLiked ? 'icon-heart' : 'icon-heart-outlined';
+export const toggleLikebutton = isLiked => {
+    const iconString = isLiked ? 'icon-heart' : 'icon-heart-outlined' //outlined when not liked
     document.querySelector('.recipe__love use').setAttribute('href', `img/icons.svg#${iconString}`);
-    // icons.svg#icon-heart-outlined
-};
+}
 
-export const toggleLikeMenu = numLikes => {
-    elements.likesMenu.style.visibility = numLikes > 0 ? 'visible' : 'hidden';
-};
-
+export const toggleLikeMenu= numLikes => {
+    elements.likesMenu.style.visibility = numLikes>0 ? 'visible' : 'hidden' ;
+}
 export const renderLike = like => {
+//copied HTML
     const markup = `
         <li>
             <a class="likes__link" href="#${like.id}">
@@ -19,16 +17,16 @@ export const renderLike = like => {
                     <img src="${like.img}" alt="${like.title}">
                 </figure>
                 <div class="likes__data">
-                    <h4 class="likes__name">${limitRecipeTitle(like.title)}</h4>
+                    <h4 class="likes__name">${limitRecipeTitle(like.title)}</h4> 
                     <p class="likes__author">${like.author}</p>
                 </div>
             </a>
         </li>
     `;
     elements.likesList.insertAdjacentHTML('beforeend', markup);
-};
-
-export const deleteLike = id => {
-    const el = document.querySelector(`.likes__link[href*="${id}"]`).parentElement;
-    if (el) el.parentElement.removeChild(el);
 }
+export const deleteLike = id => {
+    const el = document.querySelector(`.likes__link[href*="${id}"]`).parentElement; //move to parent element
+    if (el) el.parentElement.removeChild(el); //remove child
+}
+    
